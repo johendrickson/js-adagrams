cc3297e72a15096ea86235ac623064baf12a1504
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-05-21 14:03:15 -0700

    Implements wave 1. Creates an object and function for drawing 10 letters randomly.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,38 @@
-export const drawLetters = () => {
-  // Implement this method for wave 1
+const LETTER_POOL = {
+  A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2,
+  I: 9, J: 1, K: 1, L: 4, M: 2, N: 6, O: 8, P: 2,
+  Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1,
+  Y: 2, Z: 1,
 };
+
+export const drawLetters = () => {
+  const letterPool = [];
+
+  for (const [letter, count] of Object.entries(LETTER_POOL)) {
+    for (let i = 0; i < count; i++) {
+      letterPool.push(letter);
+    }
+  }
+
+  const letterBank = [];
+  const availableLetters = {...LETTER_POOL};
+
+  while (letterBank.length < 10) {
+    const randomIndex = Math.floor(Math.random() * letterPool.length);
+    const letter = letterPool[randomIndex];
+
+    if (availableLetters[letter] > 0) {
+      letterBank.push(letter);
+      availableLetters[letter]--;
+    }
+  }
+
+  return letterBank;
+};
+
+
+
+
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
