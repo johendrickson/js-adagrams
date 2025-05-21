33f3b14cbaa85703f24e566ae097b80ed784a83f
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-05-21 14:31:48 -0700

    Creates a global constant for letter scores.

--- src/adagrams.js
+++ src/adagrams.js
@@ -5,6 +5,13 @@ const LETTER_POOL = {
   Y: 2, Z: 1,
 };
 
+const LETTER_SCORES = {
+  A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4,
+  I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3,
+  Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8,
+  Y: 4, Z: 10
+};
+
 export const drawLetters = () => {
   const letterPool = [];
 
@@ -30,16 +37,47 @@ export const drawLetters = () => {
   return letterBank;
 };
 
-
-
-
-
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
+  input = input.toUpperCase();
+  lettersInHand = lettersInHand.map(letter => letter.toUpperCase());
+
+  const letterBankDict = {};
+  for (const letter of lettersInHand) {
+    if (letter in letterBankDict) {
+      letterBankDict[letter]++;
+    } else {
+      letterBankDict[letter] = 1;
+    }
+  }
+
+  const letterCount = {};
+  for (const letter of input) {
+    if (letter in letterCount) {
+      letterCount[letter]++;
+    } else {
+      letterCount[letter] =1;
+    }
+  }
+
+  for (const letter in letterCount) {
+    if (!(letter in letterBankDict)) {
+      return false;
+    } else if (letterCount[letter] > letterBankDict[letter]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  word = word.toUpperCase();
+  const totalPoints = 0;
+
+  const letterScores = {
+
+  }
 };
 
 export const highestScoreFrom = (words) => {
